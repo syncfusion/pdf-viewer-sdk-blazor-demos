53994f0423721b489ed171ea222b1baa8541829c
buildautomation <buildautomation@syncfusion.com>
2026-03-17 11:08:58 +0000

    v33.1.44

--- Common/Pages/Viewer/PdfViewer/InvisibleDigitalSignature.razor.js
+++
@@ -1,6 +0,0 @@
-﻿
-export function created() {
-    document.getElementById('pdfviewer_open').addEventListener('click', function () {
-        document.querySelector('.e-upload-browse-btn').click();
-    });
-}
--- Common/Pages/Viewer/PdfViewer/PrimaryToolbarCustomization.razor.js
+++
@@ -1,8 +0,0 @@
-﻿
-export function hidePdfviewerSearchBox(viewerID) {
-    var viewer = window.sfBlazor.getCompInstance(viewerID);
-    if (viewer && viewer.textSearchModule && viewer.textSearchModule.searchBox.style.display == "block") {
-        viewer.toolbarModule.isTextSearchBoxDisplayed = false;
-        viewer.textSearchModule.showSearchBox(false);
-    }
-}
